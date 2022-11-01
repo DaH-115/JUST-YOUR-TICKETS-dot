c7f8e739bcf9a38a65038584ebc21e5cd18716c7
dahyeon <kdh941110@gmail.com>
2022-11-01 21:24:15 +0900

    first commit

---
+++ styles/global-style.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  html {
+    box-sizing: border-box;
+    font-size: 20px;
+    min-width: 320px;
+  }
+  h1, p {
+    margin: 0
+  }
+
+  a {
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  button {
+    background: inherit; 
+    border:none; 
+    box-shadow:none; 
+    border-radius:0; 
+    padding:0; 
+    overflow:visible; 
+    cursor:pointer;
+  }
+`;
---
+++ styles/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    black: '#141414',
+    white: '#FFFFFF',
+    orange: '#F68924',
+    yellow: '#F6EE24',
+  },
+};
