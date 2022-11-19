33cc009df910f1db455d33cd0f5ef72425742254
dahyeon <kdh941110@gmail.com>
2022-11-20 00:57:27 +0900

    ✅ Header: HOME, MY TICKET LIST 메뉴 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,43 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <Link href='/search'>
-      <SearchIcon path={router.pathname}>
-        <BiSearch id='search-icon' />
-      </SearchIcon>
-    </Link>
+    <HeaderIcons>
+      <Link href='/'>
+        <HeaderLi className='home'>HOME</HeaderLi>
+      </Link>
+      <Link href='/ticket-list'>
+        <HeaderLi>MY TICKET LIST</HeaderLi>
+      </Link>
+      <Link href='/search'>
+        <SearchIcon path={router.pathname}>
+          <BiSearch id='search-icon' />
+        </SearchIcon>
+      </Link>
+    </HeaderIcons>
   );
 };
+
+const HeaderIcons = styled.ul`
+  display: flex;
+  font-weight: 700;
+  padding-left: 0.5rem;
+  background-color: ${({ theme }) => theme.colors.black};
+
+  ${({ theme }) => theme.device.desktop} {
+    position: absolute;
+    top: 3rem;
+    right: 6rem;
+  }
+`;
+
+const HeaderLi = styled.li`
+  color: #fff;
+  padding: 0.5rem;
+
+  ${({ theme }) => theme.device.desktop} {
+    color: ${({ theme }) => theme.colors.yellow};
+  }
+`;
 
 export const SearchIcon = styled.div<{ path?: string }>`
   position: fixed;
@@ -25,17 +55,20 @@ export const SearchIcon = styled.div<{ path?: string }>`
   padding: 0.5rem;
   font-size: 2rem;
   color: ${({ theme }) => theme.colors.black};
-  background-color: ${(props) =>
-    props.path === '/search'
-      ? props.theme.colors.orange
-      : props.theme.colors.yellow};
+  background-color: ${({ path, theme }) =>
+    path === '/search' ? theme.colors.orange : theme.colors.yellow};
   border-radius: 50%;
 
   #search-icon {
-    color: ${(props) =>
-      props.path === '/search' ? '#fff' : props.theme.colors.black};
+    color: ${({ path, theme }) =>
+      path === '/search' ? '#fff' : theme.colors.black};
     margin-top: 1px;
-    margin-left: 1px;
+    margin-left: 2px;
+  }
+
+  ${({ theme }) => theme.device.desktop} {
+    top: 2rem;
+    right: 2rem;
   }
 `;
 
