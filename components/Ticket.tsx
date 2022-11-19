21c76cc976ed5fa0fa73d2052d32573d3015737a
dahyeon <kdh941110@gmail.com>
2022-11-20 01:00:51 +0900

    ✨ /search/[movieTitle]: 영화 상세 페이지 추가

--- components/Ticket.tsx
+++ components/Ticket.tsx
@@ -2,26 +2,28 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Image from 'next/image';
 import styled from 'styled-components';
+import Link from 'next/link';
 import { AiOutlineArrowRight } from 'react-icons/ai';
+import { AiFillInfoCircle } from 'react-icons/ai';
 
 import MovieDetail from './MovieDetail';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 interface TicketProps {
   movieId?: number;
-  movieIndex: number;
+  movieIndex?: number;
   writeDate?: number;
   reviewText?: string;
-  voteAverage: number | string;
+  overview?: string;
+  posterPath: string | null;
   title: string;
   releaseDate: string;
-  posterPath: string | null;
+  voteAverage: number | string;
 }
 
 const Ticket = (props: TicketProps) => {
   const router = useRouter();
-  const [janre, setJanre] = useState([]);
+  const [janre, setJanre] = useState<string[]>([]);
   const movieId = props.movieId;
   const releaseYear = props.releaseDate.slice(0, 4);
   const writeDate = new Date(props.writeDate!).toLocaleDateString();
@@ -46,17 +48,38 @@ const Ticket = (props: TicketProps) => {
   return (
     <>
       <TicketWrapper>
+        <MovieIndex routePath={router.pathname}>
+          {router.pathname === '/' || router.pathname === '/search'
+            ? `${props.movieIndex}.`
+            : `${writeDate}`}
+          {router.pathname === '/ticket-list' || (
+            <Link
+              href={{
+                pathname: `/search/${props.title}`,
+                query: {
+                  title: props.title,
+                  releaseDate: releaseYear,
+                  posterImage: `https://image.tmdb.org/t/p/w500/${props.posterPath}`,
+                  voteAverage: props.voteAverage,
+                  janre,
+                  overview: props.overview,
+                },
+              }}
+              as={`/search/${props.title}`}
+            >
+              <StyeldInfo>
+                <AiFillInfoCircle />
+              </StyeldInfo>
+            </Link>
+          )}
+        </MovieIndex>
+
         {!props.posterPath ? (
           <ImgBox>
             <NoneImg>IMAGE IS NONE</NoneImg>
           </ImgBox>
         ) : (
           <ImgBox>
-            <MovieIndex routePath={router.pathname}>
-              {router.pathname === '/'
-                ? `${props.movieIndex}.`
-                : `No.${props.movieIndex} /${writeDate}`}
-            </MovieIndex>
             <Image
               src={`https://image.tmdb.org/t/p/w500/${props.posterPath}`}
               alt={props.title}
@@ -86,7 +109,7 @@ const Ticket = (props: TicketProps) => {
               as={`/write`}
             >
               <AdmitButtonWrapper>
-                <button>Admit ONE</button>
+                <button>ADMIT ONE</button>
                 <ArrowBtn>
                   <AiOutlineArrowRight />
                 </ArrowBtn>
@@ -99,51 +122,59 @@ const Ticket = (props: TicketProps) => {
   );
 };
 
-const MovieIndex = styled.div<{ routePath: string }>`
-  position: absolute;
-  top: 1.8rem;
-  left: 1rem;
-  width: 100%;
-  height: 2.3rem;
-  color: #fff;
-  font-size: ${({ routePath }) => (routePath === '/' ? '2.5rem' : '1rem')};
-  font-weight: 700;
-  padding-top: 0.8rem;
-  padding-left: 1rem;
-  background: linear-gradient(90deg, black, transparent);
-  border-top-left-radius: 1.5rem;
-  border-top-right-radius: 1.5rem;
-`;
-
 const TicketWrapper = styled.div`
   width: 360px;
-  padding-top: 1.8rem;
-  padding-bottom: 2rem;
-  padding-left: 1rem;
+  margin: 2rem 1rem 2rem 1rem;
 
   &:last-child {
     margin-right: 1rem;
   }
 
-  filter: drop-shadow(0px 0px 20px rgba(255, 255, 255, 0.1));
+  filter: drop-shadow(0px 0px 30px rgba(255, 255, 255, 0.2));
+`;
+
+const MovieIndex = styled.div<{ routePath: string }>`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 3rem;
+  color: #fff;
+  font-size: ${({ routePath }) => (routePath === '/' ? '2.5rem' : '1rem')};
+  font-weight: 700;
+  background: linear-gradient(180deg, black 30%, transparent);
+  border-top-left-radius: 1.5rem;
+  border-top-right-radius: 1.5rem;
+  padding: 0.5rem 0.8rem 0 1.4rem;
+`;
+
+const StyeldInfo = styled.div`
+  font-size: 1.8rem;
+  color: #fff;
+
+  &:active,
+  &:hover {
+    color: ${({ theme }) => theme.colors.orange};
+  }
 `;
 
 const ImgBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 360px;
   overflow: hidden;
   border-radius: 1.5rem;
 
   Img {
-    /* width: 100%; */
+    width: 100%;
     border-radius: 1.5rem;
   }
 
-  &:hover,
-  &:active {
+  &:hover {
     Img {
-      scale: 120%;
+      scale: 105%;
       transition: scale ease-in 150ms;
     }
   }
@@ -163,7 +194,7 @@ const MovieDetailWrapper = styled.div`
   bottom: 1.6rem;
   left: 0;
   display: flex;
-  width: 100%;
+  width: 360px;
   height: 10rem;
   color: ${({ theme }) => theme.colors.black};
   filter: drop-shadow(0px 0px 40px rgba(50, 50, 50, 0.9));
@@ -174,7 +205,8 @@ const AdmitButtonWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 5rem;
+
+  width: 100%;
   height: 100%;
   background-color: #fff;
   padding: 1rem;
