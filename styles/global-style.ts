39de2628552d45d977e9eac91304316f1ec19954
dahyeon <kdh941110@gmail.com>
2022-12-04 14:24:43 +0900

    💄 한글 폰트 나눔 고딕으로 변경

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -4,19 +4,19 @@ import reset from 'styled-reset';
 export const GlobalStyle = createGlobalStyle`
   ${reset}
 
-* {
-  box-sizing: border-box;
-}
-
-html {
-    font-family: 'Montserrat','Noto Sans KR', sans-serif;
-    font-size: 20px;
-    letter-spacing: -0.06em;
-    color: #141414;
-    background-color: #141414;
-    font-weight: 400;
-    min-width: 350px;
+  * {
+    box-sizing: border-box;
   }
+
+  html {
+      font-family: 'Montserrat', 'Nanum Gothic', sans-serif;
+      font-size: 20px;
+      letter-spacing: -0.06em;
+      color: #141414;
+      background-color: #141414;
+      font-weight: 400;
+      min-width: 350px;
+    }
 
   h1, p {
     margin: 0
