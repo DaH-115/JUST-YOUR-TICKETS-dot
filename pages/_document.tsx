5c3a6edaba907fc902ef82a7fd6f5b16450e8db5
dahyeon <kdh941110@gmail.com>
2022-12-07 01:55:33 +0900

    ✅ styled-components 설정 babel에서 swc를 사용하는 방식으로 변경 및 타입 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,11 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -60,27 +67,28 @@ export default class MyDocument extends Document {
     );
   }
 
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
+          enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
-
       return {
         ...initialProps,
-        styles: (
+        styles: [
           <>
-            {' '}
-            {initialProps.styles} {sheet.getStyleElement()}{' '}
-          </>
-        ),
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
       };
     } finally {
       sheet.seal();
