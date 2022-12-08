f774814b88e69854d7d26c0c58fed62355785026
dahyeon <kdh941110@gmail.com>
2022-12-08 20:42:50 +0900

    ✅ MovieDetailPage 과 UserTicketDetailPage 분리

--- pages/ticket-list/[movieTitle].tsx
+++ pages/ticket-list/[movieTitle].tsx
@@ -1,8 +1,9 @@
 import { NextPage } from 'next';
 import MovieDetailPage from '../../components/movie-detail/MovieDetailPage';
+import UserTicketDetailPage from '../../components/user-ticket-detail/UserTicketDetailPage';
 
 const TicketListDetailPage: NextPage = () => {
-  return <MovieDetailPage />;
+  return <UserTicketDetailPage />;
 };
 
 export default TicketListDetailPage;
