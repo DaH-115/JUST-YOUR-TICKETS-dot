daa30a26924efb73a083cc2685ad502c417a73b8
dahyeon <kdh941110@gmail.com>
2022-12-24 18:14:10 +0900

    🚧 bug fixing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '/',
+    unoptimized: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
