79c7303294e62fd28eff78fe7e17bccd4ef81492
Gwak Dahyun <kdh941110@gmail.com>
2024-10-15 01:40:01 +0900

    💄 반응형 디자인 적용

--- app/ui/review-details-modal.tsx
+++ app/ui/review-details-modal.tsx
@@ -1,5 +1,5 @@
-import { Review } from "app/ticket-list/page";
 import Link from "next/link";
+import { Review } from "app/ticket-list/page";
 import { IoCloseOutline, IoStar } from "react-icons/io5";
 
 type ReviewDetailsModalProps = {
@@ -22,61 +22,60 @@ export default function ReviewDetailsModal({
       }`}
     >
       <div
-        id="info-card"
-        className={`absolute w-2/5 rounded-2xl border-2 border-black bg-white transition-all duration-300 ${
+        className={`absolute w-11/12 rounded-2xl border-2 border-black bg-white drop-shadow-lg transition-all duration-500 lg:w-2/5 ${
           isModalOpen
             ? "visible translate-y-0 opacity-100"
-            : "invisible translate-y-10 opacity-0"
+            : "invisible translate-y-full opacity-0"
         }`}
       >
-        <div className="flex items-center justify-between pr-2">
-          <div className="flex items-center justify-center p-4">
-            <IoStar className="mt-2" size={18} />
-            <p className="text-4xl font-bold">{selectedReview?.rating}</p>
+        <div className="flex h-[4rem] items-center justify-between pr-2">
+          <div className="flex h-full items-center justify-center border-r border-black px-4 py-2">
+            <IoStar className="mt-0 md:mt-2" size={18} />
+            <p className="text-2xl font-bold md:text-3xl">
+              {selectedReview?.rating}
+            </p>
           </div>
-          <div className="w-full border-l-2 border-black p-2">
-            <p className="text-xs">{selectedReview?.date}</p>
-            <p className="font-bold">{selectedReview?.reviewTitle}</p>
-            <div className="flex text-sm text-gray-500">
-              <p>
-                {selectedReview?.movieTitle} - {selectedReview?.releaseYear}
-              </p>
+          <div className="w-full p-2">
+            <h1 className="font-bold">{selectedReview?.reviewTitle}</h1>
+            <div className="flex text-xs text-gray-500">
+              {selectedReview?.movieTitle} - {selectedReview?.releaseYear}
             </div>
           </div>
-          <div
-            id="btn-group"
-            className="flex items-center justify-center whitespace-nowrap text-sm"
-          >
-            <Link
-              href={`/write-review/${selectedReview?.id}?movieId=${selectedReview?.movieId}`}
-            >
-              <button className="rounded-full border-2 border-black bg-white px-4 py-2 font-bold transition-colors duration-300 hover:bg-black hover:text-white active:bg-black active:text-white">
-                수정
-              </button>
-            </Link>
+        </div>
+        <div className="h-64 flex-1 overflow-y-scroll border-y border-black px-4 pb-8 pt-2 lg:h-96">
+          <h2 className="mb-1 text-xs font-bold">리뷰 내용</h2>
+          <p className="break-keep">{selectedReview?.review}</p>
+        </div>
+        <div className="flex items-center justify-between px-4 py-2 text-sm">
+          <p className="text-xs">{selectedReview?.date}</p>
+          <div className="flex items-center">
+            <span className="mr-1 font-bold">
+              {selectedReview?.userName ? selectedReview?.userName : "Guest"}
+            </span>
+            님의 리뷰
+          </div>
+        </div>
+        <div className="flex items-center justify-between rounded-b-xl bg-black p-2">
+          <div className="flex items-center justify-center whitespace-nowrap text-xs">
             <button
               onClick={() => handleDeleteHandler(selectedReview?.id || "")}
-              className="rounded-full border-2 border-black bg-white px-4 py-2 font-bold transition-colors duration-300 hover:bg-black hover:text-white active:bg-black active:text-white"
+              className="rounded-full border-2 border-black bg-white px-2 py-1 transition-colors duration-300 hover:bg-gray-200 active:bg-black active:text-white"
             >
               삭제
             </button>
+            <Link
+              href={`/write-review/${selectedReview?.id}?movieId=${selectedReview?.movieId}`}
+            >
+              <button className="rounded-full border-2 border-black bg-white px-2 py-1 transition-colors duration-300 hover:bg-gray-200 active:bg-black active:text-white">
+                수정
+              </button>
+            </Link>
           </div>
-        </div>
-        <div className="h-96 flex-1 overflow-y-scroll border-y-2 border-black px-4 pb-6">
-          <p className="pt-4 text-xs font-bold">리뷰 내용</p>
-          <p className="break-keep">{selectedReview?.review}</p>
-        </div>
-        <div className="flex justify-end px-4 py-2">
-          <p className="mr-2 font-bold">
-            {selectedReview?.userName ? selectedReview?.userName : "Guest"}
-          </p>
-          님의 리뷰
-        </div>
-        <div
-          onClick={closeModalHandler}
-          className="flex cursor-pointer justify-end rounded-b-xl bg-black p-4 font-bold text-white"
-        >
-          <IoCloseOutline size={26} />
+          <IoCloseOutline
+            onClick={closeModalHandler}
+            className="cursor-pointer text-white"
+            size={26}
+          />
         </div>
       </div>
     </div>
