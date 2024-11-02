ae5d7e7b694a893e87888e871ba26d91e96d1019
Gwak Dahyun <kdh941110@gmail.com>
2024-11-02 21:55:42 +0900

    💄 디자인 수치 조정

--- app/my-page/side-review-list.tsx
+++ app/my-page/side-review-list.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 import { db } from "firebase-config";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { MovieReview } from "api/movie-reviews/fetchMovieReviews";
+import { firebaseErrorHandler } from "app/my-page/utils/firebase-error";
 import { IoStar } from "react-icons/io5";
-import { firebaseErrorHandler } from "./utils/firebase-error";
 import { useError } from "store/error-context";
 
 export default function SideReviewList({ uid }: { uid: string }) {
@@ -41,31 +41,31 @@ export default function SideReviewList({ uid }: { uid: string }) {
   return (
     <section className="group relative hidden lg:ml-8 lg:block lg:w-3/5">
       <div className="absolute inset-0 overflow-y-auto rounded-xl border-2 border-black bg-white p-4 px-8 transition-all duration-300 scrollbar-hide group-hover:-translate-x-1 group-hover:-translate-y-1">
-        <div className="flex items-center justify-between border-b-2 border-black pb-1">
-          <h2 className="mr-4 text-2xl font-bold">MY TICKET LIST</h2>
-          <div className="mr-4 text-nowrap text-sm">
+        <div className="flex items-center justify-between border-b-2 border-black pb-2">
+          <h2 className="text-lg font-bold">MY TICKET LIST</h2>
+          <div className="text-nowrap text-xs">
             총 <span className="font-bold">{userReviews.length}</span>개
           </div>
           <Link href={`/my-page/my-ticket-list?uid=${uid}`}>
-            <div className="text-nowrap rounded-full px-2 py-1 text-xs font-bold text-gray-500 transition-all duration-200 hover:bg-black hover:text-white focus:outline-none">
+            <div className="text-nowrap rounded-full px-2 py-1 text-xs text-black transition-all duration-300 hover:bg-black hover:text-white focus:outline-none">
               전체보기
             </div>
           </Link>
         </div>
         {userReviews && userReviews.length > 0 ? (
-          <ul className="py-4">
+          <ul className="py-1">
             {userReviews.map((review, index) => (
               <li
                 key={review.id}
-                className="flex items-center border-b-2 border-gray-300 p-2"
+                className="flex items-center border-b border-gray-300 p-2"
               >
                 <p className="mr-4 rounded-full border border-black bg-white px-2 py-1 text-xs font-bold">
                   {index + 1}
                 </p>
-                <div className="w-full">
-                  <div className="text-xs">{review.date}</div>
+                <div className="w-full text-xs">
+                  <div>{review.date}</div>
                   <h3 className="font-bold">{review.reviewTitle}</h3>
-                  <div className="flex w-full items-center justify-between text-xs text-gray-500">
+                  <div className="flex w-full items-center justify-between text-gray-500">
                     <div>{`${review.movieTitle} - ${review.releaseYear}`}</div>
                     <div className="ml-2 flex items-center justify-center font-bold">
                       <IoStar />
@@ -77,7 +77,7 @@ export default function SideReviewList({ uid }: { uid: string }) {
             ))}
           </ul>
         ) : (
-          <div className="py-4 text-sm text-gray-500">등록된 리뷰 없음</div>
+          <div className="py-4 text-xs text-gray-500">등록된 리뷰 없음</div>
         )}
       </div>
       <span className="absolute left-1 top-1 -z-10 h-full w-full rounded-xl border-2 border-black bg-black transition-all duration-300 group-hover:translate-x-1 group-hover:translate-y-1 group-hover:bg-gray-200" />
