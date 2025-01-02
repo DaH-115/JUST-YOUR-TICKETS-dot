04a3771739ead2a2f26606640edbe69aef10696f
Gwak Dahyun <kdh941110@gmail.com>
2025-01-03 08:59:16 +0900

    🔧 영화 상세 페이지에 예고편 및 비슷한 영화 데이터를 SSR로 전환

--- app/movie-details/all-movie-trailers.tsx
+++ app/movie-details/all-movie-trailers.tsx
@@ -1,31 +1,10 @@
-"use client";
+import { MovieTrailer } from "api/fetchVideosMovies";
 
-import { useCallback, useEffect, useState } from "react";
-import { fetchVideosMovies, MovieTrailer } from "api/fetchVideosMovies";
-
-export default function AllMovieTrailers({ movieId }: { movieId: number }) {
-  const [movieTrailer, setMovieTrailer] = useState<MovieTrailer[]>([]);
-  const [isError, setIsError] = useState<string | null>(null);
-  const getYouTubeUrl = useCallback(
-    (trailerKey: string) => `https://www.youtube.com/embed/${trailerKey}`,
-    [],
-  );
-
-  useEffect(() => {
-    const fetchVideos = async () => {
-      try {
-        const { results } = await fetchVideosMovies(movieId);
-        setMovieTrailer(results);
-      } catch (error) {
-        console.error(error);
-        setMovieTrailer([]);
-        setIsError("예고편을 불러오는데 실패했습니다.");
-      }
-    };
-
-    fetchVideos();
-  }, [movieId]);
-
+export default function AllMovieTrailers({
+  movieTrailer,
+}: {
+  movieTrailer: MovieTrailer[];
+}) {
   return (
     <section className="px-8 py-4 lg:p-8">
       <h2 className="mb-4 text-2xl font-bold text-white lg:mb-6">
@@ -36,7 +15,7 @@ export default function AllMovieTrailers({ movieId }: { movieId: number }) {
           movieTrailer.map((trailer) => (
             <div key={trailer.id} className="aspect-video">
               <iframe
-                src={getYouTubeUrl(trailer.key)}
+                src={`https://www.youtube.com/embed/${trailer.key}`}
                 title={trailer.name}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
@@ -45,9 +24,7 @@ export default function AllMovieTrailers({ movieId }: { movieId: number }) {
             </div>
           ))
         ) : (
-          <p className="w-full text-gray-400">
-            {isError ? isError : "등록된 예고편이 없습니다."}
-          </p>
+          <p className="w-full text-gray-400">등록된 예고편이 없습니다.</p>
         )}
       </div>
     </section>
