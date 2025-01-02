ce2084e6b570f1b2cd582c6da3895f9116b75e87
Gwak Dahyun <kdh941110@gmail.com>
2025-01-02 13:45:39 +0900

    ✅ Now Playing 부분 SSR로 처리 & 디자인 수정

--- app/search/search-page.tsx
+++ app/search/search-page.tsx
@@ -5,12 +5,12 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Movie } from "api/fetchNowPlayingMovies";
-import { fetchNowPlayingMovies } from "api/fetchNowPlayingMovies";
 import { IoSearchOutline } from "react-icons/io5";
 import { fetchSearchMovies } from "api/fetchSearchMovies";
 import ScrollToTopButton from "app/ui/scroll-to-top-button";
 import Catchphrase from "app/ui/layout/catchphrase";
 import SwiperCard from "app/ui/swiper-card";
+import { FaArrowRight } from "react-icons/fa";
 
 const searchSchema = z.object({
   query: z.string().min(1, "검색어를 입력해주세요."),
@@ -18,11 +18,13 @@ const searchSchema = z.object({
 
 type SearchSchema = z.infer<typeof searchSchema>;
 
-export default function SearchPage() {
+export default function SearchPage({
+  nowPlayingMovies,
+}: {
+  nowPlayingMovies: Movie[];
+}) {
   const [searchResults, setSearchResults] = useState<Movie[]>([]);
-  const [nowPlayingMovies, setNowPlayingMovies] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [nowPlayingMovieLoading, setNowPlayingMovieLoading] = useState(false);
   const [isError, setIsError] = useState<string | null>(null);
   const {
     register,
@@ -31,23 +33,6 @@ export default function SearchPage() {
   } = useForm<SearchSchema>({
     resolver: zodResolver(searchSchema),
   });
-
-  useEffect(() => {
-    setNowPlayingMovieLoading(true);
-
-    const fetchData = async () => {
-      try {
-        const { results } = await fetchNowPlayingMovies();
-        setNowPlayingMovies(results);
-      } catch (error) {
-        setIsError("영화 정보를 불러오는데 실패했습니다.");
-      } finally {
-        setNowPlayingMovieLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   useEffect(() => {
     if (errors.query) {
@@ -79,36 +64,33 @@ export default function SearchPage() {
 
   return (
     <>
-      <main className="pt-8">
-        <section className="mx-auto w-3/4 pb-8 md:pb-16 lg:w-2/4">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex items-center border-b border-black p-2"
-          >
+      <main className="pt-20">
+        <section className="mx-auto w-3/4 lg:w-1/3">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex items-center">
             <label htmlFor="search-input" className="sr-only">
               영화 검색
             </label>
             <input
               id="search-input"
-              className="mr-3 w-full appearance-none border-none bg-transparent pl-1 leading-tight text-gray-800 focus:outline-none"
-              type="text"
+              className="mr-2 w-full rounded-lg bg-white px-4 py-2 text-sm text-black focus:outline-none focus:ring-2 focus:ring-[#D4AF37]"
+              type="search"
               placeholder="검색어를 입력하세요"
               {...register("query")}
             />
             <button
-              className="flex cursor-pointer items-center justify-center rounded-full border border-black bg-black p-2 text-white transition-colors duration-300 ease-in-out hover:bg-white hover:text-black"
+              className="cursor-pointer rounded-full p-2 text-white transition-colors duration-300 ease-in-out hover:bg-[#D4AF37] hover:text-black"
               type="submit"
               disabled={isLoading}
             >
-              <IoSearchOutline size={20} />
+              <IoSearchOutline size={24} />
             </button>
           </form>
         </section>
 
         {isLoading && (
           <div className="flex items-center justify-center py-8 lg:py-16">
-            <p className="animate-pulse text-center text-lg font-bold text-gray-300 lg:text-2xl">
-              검색 중...
+            <p className="animate-pulse text-center text-lg font-bold text-gray-500 lg:text-2xl">
+              검색 중
             </p>
           </div>
         )}
@@ -121,8 +103,11 @@ export default function SearchPage() {
         )}
         {searchResults.length > 0 ? (
           <section className="pt-0 md:p-8">
-            <h1 className="mb-8 ml-8 text-4xl font-bold md:ml-0">검색 결과</h1>
-            <div className="grid grid-cols-1 gap-4 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
+            <h1 className="flex items-center text-2xl font-bold text-white md:text-4xl">
+              검색 결과
+              <FaArrowRight size={26} className="ml-2" />
+            </h1>
+            <div className="grid grid-cols-1 gap-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
               {searchResults.map((result, idx) => (
                 <SwiperCard
                   key={result.id}
@@ -133,32 +118,20 @@ export default function SearchPage() {
               ))}
             </div>
           </section>
-        ) : touchedFields.query ? (
-          <div className="flex items-center justify-center py-8 lg:py-16">
-            <p className="text-center text-lg font-bold text-gray-300 lg:text-2xl">
+        ) : !isLoading && touchedFields.query ? (
+          <div className="flex items-center justify-center">
+            <p className="py-8 text-center text-lg font-bold text-gray-500">
               검색 결과가 없습니다.
             </p>
           </div>
         ) : null}
       </main>
-      {nowPlayingMovieLoading && (
-        <div className="flex items-center justify-center py-8 lg:py-16">
-          <p className="animate-pulse text-center text-lg font-bold text-black lg:text-xl">
-            불러 오는 중...
-          </p>
-        </div>
-      )}
-      {isError && (
-        <div className="flex items-center justify-center py-8 lg:py-16">
-          <p className="text-center text-lg font-bold text-gray-300 lg:text-2xl">
-            {isError}
-          </p>
-        </div>
-      )}
       {!searchResults.length && (
-        <section className="bg-gray-100 p-4 md:p-8">
-          <h2 className="text-2xl font-bold md:text-4xl">추천 영화</h2>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
+        <section className="p-4 md:p-8">
+          <h2 className="flex items-center text-2xl font-bold text-[#D4AF37] md:text-4xl">
+            Now Playing <FaArrowRight size={28} className="ml-2 mt-4" />
+          </h2>
+          <div className="grid grid-cols-1 gap-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
             {nowPlayingMovies.map((movie, idx) => (
               <SwiperCard
                 key={movie.id}
