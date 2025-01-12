75c30cdcc09b7bdf40d0769443b64cbd88fac735
Gwak Dahyun <kdh941110@gmail.com>
2025-01-13 02:13:33 +0900

    💄 메타데이터 업데이트 및 아이콘 설정 개선

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,29 +12,89 @@ export const metadata: Metadata = {
     canonical: "/",
   },
   title: {
-    template: "%s | Just Your Tickets",
-    default: "Just Your Tickets",
+    template: "%s | Just Your Tickets.",
+    default: "Just Your Tickets.",
   },
   description: "Make a ticket for your own movie ticket.",
   keywords: ["movie", "ticket", "booking"],
   openGraph: {
-    title: "Just Your Tickets",
+    title: "Just Your Tickets.",
     description: "Make a ticket for your own movie ticket.",
     url: "/",
-    siteName: "Just Your Tickets",
+    siteName: "Just Your Tickets.",
     images: [
       {
         url: "/images/og-card.jpg",
         width: 1200,
         height: 630,
-        alt: "Just Your Tickets Preview",
+        alt: "Just Your Tickets.",
       },
     ],
   },
   icons: {
-    icon: "/images/favicon.ico",
+    apple: [
+      {
+        url: "/images/apple-icon-57x57.png",
+        sizes: "57x57",
+        type: "image/png",
+      },
+      {
+        url: "/images/apple-icon-60x60.png",
+        sizes: "60x60",
+        type: "image/png",
+      },
+      {
+        url: "/images/apple-icon-72x72.png",
+        sizes: "72x72",
+        type: "image/png",
+      },
+      {
+        url: "/images/apple-icon-76x76.png",
+        sizes: "76x76",
+        type: "image/png",
+      },
+      {
+        url: "/images/apple-icon-114x114.png",
+        sizes: "114x114",
+        type: "image/png",
+      },
+      {
+        url: "/images/apple-icon-120x120.png",
+        sizes: "120x120",
+        type: "image/png",
+      },
+      {
+        url: "/images/apple-icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        url: "/images/apple-icon-152x152.png",
+        sizes: "152x152",
+        type: "image/png",
+      },
+      {
+        url: "/images/apple-icon-180x180.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    icon: [
+      { url: "/images/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/images/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/images/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      {
+        url: "/images/android-icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
     shortcut: "/images/favicon.ico",
-    apple: "/images/apple-icon.png",
+  },
+  manifest: "/manifest.json",
+  other: {
+    "msapplication-TileColor": "#ffffff",
+    "msapplication-TileImage": "/images/ms-icon-144x144.png",
   },
 };
 
